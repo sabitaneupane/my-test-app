63c01c5aad71490a31f436fdb0948e3eb6cb911f
Sabita Neupane <nsabita.sasa@gmail.com>
2021-05-05 16:57:29 +0545

    duplicate file

---
+++ src/notfound copy.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const NotFound: React.FC<{}> = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col text-center">
+          <h4 className="mt-6">404 Resource Not Found</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
